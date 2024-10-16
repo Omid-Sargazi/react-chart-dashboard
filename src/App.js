64ec900e7f3b188dcf07743f808835fd51accfbe
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-16 23:17:04 +0330

    App for chart react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary>
+        <h1>Interactive Dashboard</h1>
+        <Dashboard />
+      </ErrorBoundary>
     </div>
   );
 }
